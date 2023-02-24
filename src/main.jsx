f931c5a7daea76b27e425d6f057a871235020335
d4nQw3rty <danlinmoy@gmail.com>
2023-02-24 18:01:36 -0400

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+/*import { Provider } from 'react-redux';*/
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
 );
