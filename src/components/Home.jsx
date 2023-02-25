fd70984ba61ad18f50c9de794593e06abd0f1691
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-24 22:32:33 -0600

    Add home elements 🏠

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import img from '../assets/img/marriot-wobg.png';
+import home from './Home.module.css';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className={home.background}>
+    <img src={img} alt="Logo" className={home.hotel}/>
+    <h1 className={home.header}>The new Marriott Hotel</h1>
+    <button type='button' className={home.loginBtn}>Login</button>
+    <button type='button' className={home.signupBtn}>Sign up</button>
   </div>
 );
 
