a3a925c14495f09e27bbb2ad7112a0588d8ce9ed
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-26 17:44:51 -0600

    Add home screen styles and content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import home from './Home.module.css';
 
 const Home = () => (
   <div className={home.background}>
-    <img src={img} alt="Logo" className={home.hotel}/>
+    <img src={img} alt="Logo" className={home.hotel} />
     <h1 className={home.header}>The new Marriott Hotel</h1>
-    <button type='button' className={home.loginBtn}>Login</button>
-    <button type='button' className={home.signupBtn}>Sign up</button>
+    <button type="button" className={home.loginBtn}>Login</button>
+    <button type="button" className={home.signupBtn}>Sign up</button>
   </div>
 );
 
