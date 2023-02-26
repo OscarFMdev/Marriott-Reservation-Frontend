747df29300f1bc36dc767c33ac4966e366e390d1
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-26 17:45:26 -0600

    Add mani styles and update content

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 /* import { Provider } from 'react-redux'; */
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './main.css'
+import './main.css';
 
 createRoot(document.getElementById('root')).render(
 
@@ -11,4 +11,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>,
 );
-
