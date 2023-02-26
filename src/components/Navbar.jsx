600dada6fb53b1f6dbdc913d7f60f960e940aca4
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-26 17:45:08 -0600

    Add navbar content and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,47 @@
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import navbar from './Navbar.module.css';
+import logo from '../assets/img/marriott-logo-mobile.png';
 
 const handleActive = ({ isActive }) => (isActive
   ? {
-    color: 'blue',
-    textDecoration: 'underline',
+    backgroundColor: '#95C010',
+    color: 'white',
+    textDecoration: 'none',
   }
-  : { });
+  : { 
+    color: 'black',
+  });
 
-const NavBar = () => (
-  <header>
-    <NavLink to="/">
-      <h1>Hotel Marriott</h1>
-    </NavLink>
-    <nav>
-      <NavLink
-        to="/"
-        style={handleActive}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/Rooms"
-        style={handleActive}
-      >
-        Rooms
-      </NavLink>
-      <NavLink
-        to="/Reservation"
-        style={handleActive}
-      >
-        Reservation
-      </NavLink>
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <nav className={navbar.nav}>
+      <div className={`${navbar.menuIcon} ${isOpen ? navbar.open : ''}`} onClick={toggleMenu}>
+        <div className={`${isOpen ? navbar.menuIconCross : navbar.menuIconLines} ${isOpen ? navbar.firstBar : ''}`}></div>
+        <div className={`${isOpen ? navbar.menuIconCross : navbar.menuIconLines} ${isOpen ? navbar.secondBar : ''}`}></div>
+      </div>
+      <div className={`${navbar.menuItems} ${isOpen ? navbar.open : ''}`}>
+        <NavLink to="/">
+          <img src={logo} alt="Logo" className={navbar.logo} onClick={toggleMenu}/>
+        </NavLink>
+        <NavLink to="/" style={handleActive} className={navbar.link} onClick={toggleMenu}>
+          Home
+        </NavLink>
+        <NavLink to="/Rooms" style={handleActive} className={navbar.link} onClick={toggleMenu}>
+          Rooms
+        </NavLink>
+        <NavLink to="/Reservation" style={handleActive} className={navbar.link} onClick={toggleMenu}>
+          Reservation
+        </NavLink>
+      </div>
     </nav>
-  </header>
-);
+  );
+};
 
 export default NavBar;
