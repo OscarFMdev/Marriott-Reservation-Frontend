8c7198b6a3863e6e957c7f581dcdd915e58ba72d
d4nQw3rty <danlinmoy@gmail.com>
2023-02-27 15:07:01 -0400

    add login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
+import Loginpage from './pages/Loginpage';
+import SignUpPage from './pages/SignUpPage';
 import Roomspage from './pages/Roomspage';
 import Reservationspage from './pages/Reservationspage';
+
 import './App.css';
 
 const App = () => (
@@ -10,8 +13,11 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Loginpage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/rooms" element={<Roomspage />} />
       <Route path="/reservation" element={<Reservationspage />} />
+
     </Routes>
   </main>
 
