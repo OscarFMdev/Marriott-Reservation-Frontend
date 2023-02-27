f932e4d3bb8234b0610153a37d3607c586457819
d4nQw3rty <danlinmoy@gmail.com>
2023-02-27 15:08:22 -0400

    signup component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
+const SignUp = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignUp = () => {
+    console.table(fullName, email, password, confirmPassword);
+  };
+
+  return (
+    <div className="flex flex-column">
+      <h1>Sign Up</h1>
+      .
+      <InputText placeholder="Full Name" value={fullName} required onChange={(e) => setFullName(e.target.value)} />
+      <InputText placeholder="Email" type="email" value={email} required onChange={(e) => setEmail(e.target.value)} />
+      <InputText placeholder="Password" type="password" value={password} required onChange={(e) => setPassword(e.target.value)} />
+      <InputText placeholder="Confirm Password" type="password" value={confirmPassword} required onChange={(e) => setConfirmPassword(e.target.value)} />
+
+      <Button onClick={handleSignUp}>Sign Up</Button>
+    </div>
+  );
+};
+
+export default SignUp;
