4034db9686d5ec352d0da4b218772b99897d0262
d4nQw3rty <danlinmoy@gmail.com>
2023-02-27 15:09:43 -0400

    create loginslice

---
+++ src/redux/loginSlice.jsx
@@ -0,0 +1,58 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: '',
+  token: '',
+  loading: false,
+
+};
+
+const loginUser = createAsyncThunk('user', async (body) => {
+  const response = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Autorization: localStorage.getItem('token'),
+    },
+    body: JSON.stringify(body),
+  });
+
+  return response.json();
+});
+
+const loginSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addToken: (state) => {
+      const s = state;
+      s.token = localStorage.getItem('token');
+    },
+    addUser: (state) => {
+      const s = state;
+      s.user = localStorage.getItem('user');
+    },
+  },
+  extraReducers: {
+    [loginUser.pending]: (state) => {
+      const s = state;
+      s.loading = true;
+    },
+    [loginUser.fulfilled]: (state, { payload: { user, token } }) => {
+      const s = state;
+      s.loading = false;
+      s.token = token;
+      s.user = user;
+      localStorage.setItem('token', JSON.stringify(token));
+      localStorage.setItem('user', JSON.stringify(user));
+    },
+    [loginUser.rejected]: (state) => {
+      const s = state;
+      s.loading = false;
+    },
+  },
+});
+
+export const { addToken, addUser } = loginSlice.actions;
+export { loginUser };
+export default loginSlice.reducer;
