f74f1c2f9e4d40643417746f2a85e671332887ef
d4nQw3rty <danlinmoy@gmail.com>
2023-02-27 16:33:19 -0400

    Create signup slice

--- src/redux/signUpSlice.jsx
+++ src/redux/signUpSlice.jsx
@@ -8,15 +8,44 @@ const initialState = {
   error: '',
 };
 
+const signUpUser = createAsyncThunk('userSignUp', async (body) => {
+  const response = await fetch('http://localhost:3000/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  return response.json();
+});
+
 const signUpSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
   },
   extraReducers: {
-
+    [signUpUser.pending]: (state) => {
+      const s = state;
+      s.loading = true;
+    },
+    [signUpUser.fulfilled]: (state, { payload: { msg, error } }) => {
+      const s = state;
+      s.loading = false;
+      if(error){
+        s.error = error;
+      }
+      else{
+        s.msg = msg;
+      }          
+    },
+    [signUpUser.rejected]: (state) => {
+      const s = state;
+      s.loading = true;
+    }
   },
-
 });
 
 export default signUpSlice.reducer;
+export { signUpUser };
