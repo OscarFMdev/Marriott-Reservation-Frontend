04710dce7f34ea14a116b6fbdd9e2ce5b818995c
d4nQw3rty <danlinmoy@gmail.com>
2023-02-27 15:07:27 -0400

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-/* import { Provider } from 'react-redux'; */
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
