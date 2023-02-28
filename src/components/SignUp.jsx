bb5f37d722ae5569f3600fab976c95945c068c31
d4nQw3rty <danlinmoy@gmail.com>
2023-02-28 19:01:59 -0400

    Sign up user

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,34 +1,73 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';        
+import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import { useDispatch } from 'react-redux';
 import { signUpUser } from '../redux/signUpSlice';
 
 const SignUp = () => {
-  const [fullName, setFullName] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const dispatch = useDispatch();
 
-  const handleSignUp = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     dispatch(signUpUser({
-      fullName, email, password, confirmPassword,
+      user: {
+        name,
+        email,
+        password,
+        passwordConfirmation,
+      },
     }));
   };
 
   return (
-    <div className="flex flex-column align-items-center">
-      <h1>SignUp</h1>      .
-      <InputText placeholder="Full Name" value={fullName} required onChange={(e) => setFullName(e.target.value)} />
-      <InputText placeholder="Email" type="email" value={email} required onChange={(e) => setEmail(e.target.value)} />
-      <Password placeholder="Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
-      <Password placeholder="Confirm Password" value={confirmPassword} required onChange={(e) => setConfirmPassword(e.target.value)} />
-      
-     
-      <Button label="SignUp" className="p-button-success" onClick={handleSignUp} />
+    <div className="p-grid p-justify-center">
+      <div className="p-col-12 p-md-8 p-lg-6">
+        <div className="card">
+          <h1>Sign Up</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="p-field">
+              <InputText
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                label="Name"
+                placeholder='Full Name'
+              />
+
+              <InputText
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label="Email"
+                placeholder='Email'
+              />
+
+              <Password
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+              />
+
+              <Password
+                id="password_confirmation"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                placeholder='Password Confirmation'
+              />
+
+              <Button label="Sign Up" type="submit" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
