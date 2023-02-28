22b6d83fb38f0a9c6fdcaa173991bf8f910d9829
d4nQw3rty <danlinmoy@gmail.com>
2023-02-28 08:23:59 -0400

    Change  form passwords fields

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';        
 import { Button } from 'primereact/button';
 import { useDispatch } from 'react-redux';
 import { signUpUser } from '../redux/signUpSlice';
@@ -19,14 +20,14 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex flex-column">
-      <h1>Sign Up</h1>
-      .
+    <div className="flex flex-column align-items-center">
+      <h1>Sign Up</h1>      .
       <InputText placeholder="Full Name" value={fullName} required onChange={(e) => setFullName(e.target.value)} />
       <InputText placeholder="Email" type="email" value={email} required onChange={(e) => setEmail(e.target.value)} />
-      <InputText placeholder="Password" type="password" value={password} required onChange={(e) => setPassword(e.target.value)} />
-      <InputText placeholder="Confirm Password" type="password" value={confirmPassword} required onChange={(e) => setConfirmPassword(e.target.value)} />
-
+      <Password placeholder="Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
+      <Password placeholder="Confirm Password" value={confirmPassword} required onChange={(e) => setConfirmPassword(e.target.value)} />
+      
+     
       <Button onClick={handleSignUp}>Sign Up</Button>
     </div>
   );
