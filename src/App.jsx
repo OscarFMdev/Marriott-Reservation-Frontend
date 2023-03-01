8402339ab015b205badd44ad1cc3ed21dd302711
d4nQw3rty <danlinmoy@gmail.com>
2023-03-01 17:29:27 -0400

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import './App.css';
 import MyBookingspage from './pages/MyBookingspage';
 import AddRoompage from './pages/AddRoompage';
 import DeleteRoompage from './pages/DeleteRoompage';
-import { AuthProvider } from 'react-auth-kit'
 
 const App = () => (
 
@@ -22,11 +21,6 @@ const App = () => (
       {window.location.pathname !== '/' && <Navbar />}
     </div>
     <main className="content">
-    <AuthProvider authType = {'cookie'}
-                  authName={'_auth'}
-                  cookieDomain={window.location.hostname}
-                  cookieSecure={window.location.protocol === "https:"}>
-
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Loginpage />} />
@@ -37,7 +31,6 @@ const App = () => (
         <Route path="/addroom" element={<AddRoompage />} />
         <Route path="/deleteroom" element={<DeleteRoompage />} />
       </Routes>
-      </AuthProvider>
     </main>
   </div>
 
