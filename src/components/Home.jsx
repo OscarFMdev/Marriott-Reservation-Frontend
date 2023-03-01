70259297ae1a064952cb86492d79ba9b53db75b6
d4nQw3rty <danlinmoy@gmail.com>
2023-03-01 16:34:04 -0400

    add links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import img from '../assets/img/marriot-wobg.png';
 import home from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
   <div className={home.background}>
     <img src={img} alt="Logo" className={home.hotel} />
     <h1 className={home.header}>The new Marriott Hotel</h1>
+    <Link to="/login">
     <button type="button" className={home.loginBtn}>Login</button>
+    </Link>
+    <Link to="/signup">
     <button type="button" className={home.signupBtn}>Sign up</button>
+    </Link>
   </div>
 );
 
