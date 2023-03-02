73af092e25bf9381a7bb8676ace2607e9ca33c57
d4nQw3rty <danlinmoy@gmail.com>
2023-03-02 11:00:30 -0400

    Update rooms to fetch rooms

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchRooms } from '../redux/reducer/roomSlice';
 
-const Rooms = () => (
-  <div>
-    <h1>Rooms</h1>
-  </div>
-);
+
+
+const Rooms = () => {
+  const dispatch = useDispatch();
+  dispatch(fetchRooms());
+  return (
+    <div>
+      <h1>Rooms</h1>
+    </div>
+  );
+};
+
+
+
+
 
 export default Rooms;
