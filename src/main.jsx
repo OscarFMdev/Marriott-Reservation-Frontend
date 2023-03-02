1e00b88b4b456acfdbd2be6845c190e4a5502444
Miliyonayalew <miliayalew@gmail.com>
2023-03-03 00:03:58 +0300

    Update store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './store';
+import store from './redux/configureStore';
 import App from './App';
 import './main.css';
 
