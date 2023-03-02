0c4318a4429cc1de76266d8bb0b233be586b9bb8
Miliyonayalew <miliayalew@gmail.com>
2023-03-03 00:01:53 +0300

    Delete old store and create configure store

--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -1,1 +1,11 @@
-/* Here will be all the imports for the reducers */
+import { configureStore } from '@reduxjs/toolkit';
+
+import auth from './reducer/Auth/auth';
+
+const store = configureStore({
+  reducer: {
+    auth,
+  },
+});
+
+export default store;
