7643a85f3fafb32105e1141424d63ba34bbdfa55
d4nQw3rty <danlinmoy@gmail.com>
2023-03-03 12:01:29 -0400

    Create Details component

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
 
-const Details = () => (
-  <div>
-    <h1>Details</h1>
-  </div>
-);
+const Details = () => {
+
+  const params = useParams();
+  const rooms = useSelector((state) => state.rooms);
+  const room = rooms.rooms.find((r) => r.id === parseInt(params.id, 10));
+  console.log(room);
+
+  return (
+    <div>
+      <h1 >{room.name}</h1>
+     <p>{room.type} </p>
+      <img src={room.image} alt="" />
+      <p>{room.description}</p>
+      <p>{room.price}</p>      
+
+      <Link to="/rooms">
+        <button type="button">Back</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Details;
