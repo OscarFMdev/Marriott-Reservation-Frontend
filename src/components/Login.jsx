6674ef757af55c3f7521d957e8a18c05496713d5
Miliyonayalew <miliayalew@gmail.com>
2023-03-03 19:00:50 +0300

    Add message and navlink after login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
@@ -10,6 +10,8 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { status, message } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,10 +22,17 @@ const Login = () => {
     dispatch(signIn(user));
   };
 
+  useEffect(() => {
+    if (message === 'Logged in succesfully.') {
+      navigate('/rooms');
+    }
+  }, [message, navigate]);
+
   return (
     <div className="p-grid p-justify-center">
       <div className="p-col-12 p-md-8 p-lg-6">
         <div className="card">
+          { status === 'failed' && <p>{message}</p> }
           <h1>Login</h1>
           <form onSubmit={handleSubmit}>
             <div className="p-field">
@@ -35,6 +44,7 @@ const Login = () => {
                 label="Email"
                 placeholder="Email"
                 icon="pi pi-envelope"
+                required
               />
             </div>
 
@@ -45,6 +55,7 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
                 feedback={false}
+                required
               />
             </div>
 
