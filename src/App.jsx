595a7cc17d40340e9a87196368904fa8e02c5140
Miliyonayalew <miliayalew@gmail.com>
2023-03-03 18:59:04 +0300

    Add the protected routes to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
 import Loginpage from './pages/Loginpage';
 import SignUpPage from './pages/SignUpPage';
 import Roomspage from './pages/Roomspage';
+import Homepage from './pages/Homepage';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import '../node_modules/primeflex/primeflex.css';
 import Bookingpage from './pages/Bookingpage';
+import ProtectRoutes from './components/ProtectRoutes';
 import './App.css';
 import MyBookingspage from './pages/MyBookingspage';
 import AddRoompage from './pages/AddRoompage';
@@ -26,10 +27,12 @@ const App = () => (
         <Route path="/login" element={<Loginpage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/rooms" element={<Roomspage />} />
-        <Route path="/booking" element={<Bookingpage />} />
-        <Route path="/mybookings" element={<MyBookingspage />} />
-        <Route path="/addroom" element={<AddRoompage />} />
-        <Route path="/deleteroom" element={<DeleteRoompage />} />
+        <Route element={<ProtectRoutes />}>
+          <Route path="/booking" element={<Bookingpage />} />
+          <Route path="/mybookings" element={<MyBookingspage />} />
+          <Route path="/addroom" element={<AddRoompage />} />
+          <Route path="/deleteroom" element={<DeleteRoompage />} />
+        </Route>
       </Routes>
     </main>
   </div>
