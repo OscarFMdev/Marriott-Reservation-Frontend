31d8fd194b0dc260daf8df48d583f650bd982e39
Miliyonayalew <miliayalew@gmail.com>
2023-03-03 18:59:47 +0300

    Add navlink to the home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import img from '../assets/img/marriott-wobg.png';
 import home from './Home.module.css';
 
@@ -6,8 +7,12 @@ const Home = () => (
   <div className={home.background}>
     <img src={img} alt="Logo" className={home.hotel} />
     <h1 className={home.header}>The new Marriott Hotel</h1>
-    <button type="button" className={home.loginBtn}>Login</button>
-    <button type="button" className={home.signupBtn}>Sign up</button>
+    <NavLink to="/login">
+      <button type="button" className={home.loginBtn}>Login</button>
+    </NavLink>
+    <NavLink to="/signup">
+      <button type="button" className={home.signupBtn}>Sign up</button>
+    </NavLink>
   </div>
 );
 
