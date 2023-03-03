d5c3ad5798f3f2bb8ea66728117d2de1562fde5c
d4nQw3rty <danlinmoy@gmail.com>
2023-03-03 12:00:11 -0400

    Add rooms reducer to store

--- src/store.jsx
+++ src/store.jsx
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginSlice from './redux/loginSlice';
 import signUpSlice from './redux/signUpSlice';
-import roomSlice from './redux/reducer/roomSlice';
+import roomsReducer from './redux/roomSlice';
 
 const store = configureStore({
   reducer: {
-     loginSlice,
-     signUpSlice,
-     roomSlice
+    loginSlice,
+    signUpSlice,
+    rooms: roomsReducer,
     // reservation: reservationReducer,
   },
 });
