d6c6c33ceac19a255f993fb993b9a7127a87f72e
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-03-04 08:49:26 -0600

    Update navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const handleActive = ({ isActive }) => (isActive
     backgroundColor: '#95C010',
     color: 'white',
     textDecoration: 'none',
-    transition: '0.5s linear',
+    transition: '0.5s ease-out',
   }
   : {
     color: 'black',
