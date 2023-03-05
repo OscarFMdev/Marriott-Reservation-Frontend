03fcf0fd8940674ad2a5aa66ca5a3d17df1cbcc1
Miliyonayalew <miliayalew@gmail.com>
2023-03-06 01:19:35 +0300

    Add reducer to configure store

--- src/redux/configureStore.jsx
+++ src/redux/configureStore.jsx
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import auth from './reducer/Auth/auth';
+import auth, { getUser } from './reducer/Auth/auth';
+import reservationReducer from './reducer/reservation/reservationSlice';
 import roomsReducer from './reducer/Rooms/roomSlice';
 
 const store = configureStore({
   reducer: {
     auth,
     rooms: roomsReducer,
+    reservations: reservationReducer,
   },
 });
 
+store.dispatch(getUser());
 export default store;
