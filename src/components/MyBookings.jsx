33883086843915e3831c8e17ee89d6ac266d043a
Miliyonayalew <miliayalew@gmail.com>
2023-03-06 23:07:37 +0300

    render my booking

--- src/components/MyBookings.jsx
+++ src/components/MyBookings.jsx
@@ -1,13 +1,64 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBookings, deleteBooking, popBook } from '../redux/reducer/reservation/reservationSlice';
 
 const MyBookings = () => {
-  const { message } = useSelector((state) => state.reservations);
+  const { message, bookings } = useSelector((state) => state.reservations);
+  const { rooms } = useSelector((state) => state.rooms);
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleCancleReservation = (id) => {
+    const deleteBook = {
+      id,
+      userId: user.id,
+    };
+    dispatch(deleteBooking(deleteBook));
+    dispatch(popBook(id));
+  };
+
+  useEffect(() => {
+    dispatch(fetchBookings(user.id));
+  }, []);
 
   return (
     <>
       <h1>My Bookings</h1>
       <p>{message}</p>
+      { bookings.length === 0 ? <p>You have no bookings</p> : (
+        bookings.map((booking) => (
+          <div key={booking.id}>
+            {rooms.map((room) => {
+              if (booking.room_id === room.id) {
+                return (
+                  <div key={room.id}>
+                    <p>
+                      Room Name
+                      {room.name}
+                    </p>
+                    <p>
+                      Reservation Start Date
+                      {booking.start_date}
+                    </p>
+                    <p>
+                      Reservation End Date
+                      {booking.end_date}
+                    </p>
+                  </div>
+                );
+              }
+              return null;
+            })}
+            <button
+              type="button"
+              onClick={() => handleCancleReservation(booking.id)}
+            >
+              Cancel Reservation
+            </button>
+
+          </div>
+        ))
+      )}
     </>
   );
 };
