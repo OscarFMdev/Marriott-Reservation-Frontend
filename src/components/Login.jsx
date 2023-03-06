800f29b1997ba2a2cacf492aee3341e298cab3ce
Hans Zizold <hzizold@hotmail.com>
2023-03-06 18:22:53 -0400

    Home Page tests

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -66,7 +66,7 @@ const Login = () => {
             </div>
             <Button type="submit" label="Login" className="bg-green-500 border-200" />
             <p>
-              Don`&apos;`t Have an Account?
+              Don't Have an Account?
               <span>
                 <NavLink to="/signup"> Sign Up</NavLink>
               </span>
