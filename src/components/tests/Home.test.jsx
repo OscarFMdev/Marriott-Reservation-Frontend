15cedc408d473c9dfed757efc1bf5a7bc5287f74
Hans Zizold <hzizold@hotmail.com>
2023-03-07 17:40:45 -0400

    Home page tests

--- src/components/tests/Home.test.jsx
+++ src/components/tests/Home.test.jsx
@@ -1,19 +1,19 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
-import Home from '../Home'
-import SignUp from '../SignUp'
-import Login from '../Login'
-import store from '../../redux/configureStore'
+import Home from '../Home';
+import SignUp from '../SignUp';
+import Login from '../Login';
+import store from '../../redux/configureStore';
 
 describe('It displays the homepage title', () => {
   test('Homepage title', () => {
     render(<Router><Home /></Router>);
     expect(screen.getByText('The new Marriott Hotel')).toBeDefined();
-  })
-})
+  });
+});
 
 describe('Test redirection of the login and signup buttons', () => {
   beforeEach(() => {
@@ -24,18 +24,18 @@ describe('Test redirection of the login and signup buttons', () => {
           <Login />
           <SignUp />
         </Router>
-      </Provider>
+      </Provider>,
     );
-  })
+  });
   test('Log in Redirection', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Log in' }));
     expect(screen.getByText("Don't Have an Account?")).toBeDefined();
-  })
+  });
   test('Sign Up Redirection', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Sign up' }));
     expect(screen.getByText('Already have an account?')).toBeDefined();
-  })
-})
+  });
+});
 
 test('Home Page renders correctly', () => {
   const tree = renderer.create(
@@ -43,7 +43,7 @@ test('Home Page renders correctly', () => {
       <Router>
         <Home />
       </Router>
-    </Provider>
+    </Provider>,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
