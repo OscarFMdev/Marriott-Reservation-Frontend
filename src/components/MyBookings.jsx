5efeffc33790c24c007b6d4c85d4231866844020
Miliyonayalew <miliayalew@gmail.com>
2023-03-07 15:33:12 +0300

    Modify the booking page functionalities

--- src/components/MyBookings.jsx
+++ src/components/MyBookings.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchBookings, deleteBooking, popBook } from '../redux/reducer/reservation/reservationSlice';
-import mybookings from './TableStyles.module.css'
+import { setMessageEmpty } from '../redux/reducer/Rooms/roomSlice';
+import mybookings from './TableStyles.module.css';
 
 const MyBookings = () => {
   const { message, bookings } = useSelector((state) => state.reservations);
-  const { rooms } = useSelector((state) => state.rooms);
+  // const { rooms } = useSelector((state) => state.rooms);
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
@@ -22,68 +23,69 @@ const MyBookings = () => {
     dispatch(fetchBookings(user.id));
   }, []);
 
-  const [showMessage, setShowMessage] = useState(false);
+  const handleMessage = () => {
+    setTimeout(() => {
+      if (message === 'Room Created Successfully') dispatch(setMessageEmpty(''));
+    }, 3000);
+  };
 
   useEffect(() => {
-    if (message) {
-      setShowMessage(true);
-      setTimeout(() => {
-        setShowMessage(false);
-      }, 3000);
-    }
+    handleMessage();
   }, [message]);
 
   return (
     <main className={mybookings.container}>
       <h1>My Bookings</h1>
-      {showMessage && (
-        <p className={ showMessage ? `${mybookings.success} ${mybookings.fadeIn}` : mybookings.fadeOut}>
+      {message && (
+        <p className={message ? `${mybookings.success} ${mybookings.fadeIn}` : mybookings.fadeOut}>
           {message}
         </p>
       )}
-      <table className=''>
-      <thead>
-        <tr>
-          <th>Room Name</th>
-          <th>Start Date</th>
-          <th>End Date</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {bookings.length === 0 ? (
+      <table className="">
+        <thead>
           <tr>
-            <td colSpan="4">You have no bookings</td>
+            <th>Room Name</th>
+            <th>Check-in Date</th>
+            <th>Check-out Date</th>
+            <th>Actions</th>
           </tr>
-        ) : (
-          bookings.map((booking) => (
-            <tr key='hello' className='hello'>
-              {rooms.map((room) => {
-                if (booking.room_id === room.id) {
-                  return (
-                    <>
-                      <td>{room.name}</td>
-                      <td>{booking.start_date}</td>
-                      <td>{booking.end_date}</td>
-                    </>
-                  );
-                }
-                return null;
-              })}
-              <td>
-                <button
-                  type="button"
-                  onClick={() => handleCancelReservation(booking.id)}
-                  className={mybookings.deleteCancelBtn}
-                >
-                  Cancel
-                </button>
-              </td>
+        </thead>
+        <tbody>
+          {bookings.length === 0 ? (
+            <tr>
+              <td colSpan="4">You have no bookings</td>
             </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+          ) : (
+            bookings.map(
+              ({
+                id: bookingId,
+                start_date: startDate,
+                end_date: endDate,
+                room: {
+                  name,
+                },
+              }) => (
+                <tr key={bookingId} className="hello">
+                  <>
+                    <td>{name}</td>
+                    <td>{startDate}</td>
+                    <td>{endDate}</td>
+                  </>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => handleCancelReservation(bookingId)}
+                      className={mybookings.deleteCancelBtn}
+                    >
+                      Cancel
+                    </button>
+                  </td>
+                </tr>
+              ),
+            )
+          )}
+        </tbody>
+      </table>
 
     </main>
   );
